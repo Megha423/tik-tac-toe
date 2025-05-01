5367f124ee60274ef2e39238a2e85b0c584889dd
Megha423 <meghasawant114@gmail.com>
2025-05-01 16:30:38 +0530

    Add files via upload

---
+++ colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color(0xffb43939);
+  static Color secondaryColor = Color(0xffe5cc82);
+  static Color accentColor = Color(0xff2f948b);
+}
